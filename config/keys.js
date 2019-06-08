ef4982cc3f97b2d62f774456ae4d1323199b5bd7
Yashas Reddy <yashassp@protonmail.com>
2019-06-08 15:47:31 +0530

    important keys

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+	mongoURI: 'mongodb+srv://yashas:yashas@main-w8aag.mongodb.net/test?retryWrites=true&w=majority',
+	googleClientID: '550288029291-d6dq4a73vjpgaup1m89bea1ok6t9t8r7.apps.googleusercontent.com',
+	googleClientSecret: 'DI2I2mvoOt7IzkPqDSrAxEoD'
+};
